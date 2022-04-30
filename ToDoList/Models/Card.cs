fbf45589b730c1d4c8fa0d7b271b53881954b247
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-04-30 16:56:26 +0300

    Card Created

---
+++ ToDoList/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Card
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Id { get; set; }
+        public int Size { get; set; }
+        public Card(string title, string content, int id, int size)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Id = id;
+            this.Size = size;
+        }
+    }
+}
