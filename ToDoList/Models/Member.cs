0f007464a96ff8a6907571f5d04419d39a699caf
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-04-30 17:27:35 +0300

    Model/Member, MemberList, Size Created

---
+++ ToDoList/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public Member(int id, string fullName)
+        {
+            this.Id = id;
+            this.FullName = fullName;
+        }
+
+    }
+}
---
+++ ToDoList/Models/MemberList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class MemberList
+    {
+        public List<Member> all = new List<Member>();
+
+        public MemberList()
+        {
+            all.Add(new Member(1, "Serkan Özsöz"));
+            all.Add(new Member(2, "Kazım Koyuncu"));
+            all.Add(new Member(3, "Lorem Ipsum"));
+            all.Add(new Member(4, "Amber Heard"));
+        }
+
+    }
+}
