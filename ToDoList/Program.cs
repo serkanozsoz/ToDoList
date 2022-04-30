6ded70c2c8730d51ac127c35c46463164d0a3f4d
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-04-30 19:45:41 +0300

    moveCard Updated

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -140,6 +140,107 @@ namespace ToDoList
             Console.WriteLine("İşlem başarılı bir şekilde tamamlandı.");
         }
 
+        private static void moveCard()
+        {
+            //4 işlem: index al, kartı al, eski karttan sil ve yeniye ekle
+            //string _title, _line = String.Empty;
+            string _title = "";
+            string _line = "";
+            Card _card = new Card(null, null, -1, -1);
+            int index = -1;
+            Console.WriteLine("Öncelikle taşımak istediğiniz kartı seçmeniz gerekiyor. ");
+            Console.WriteLine("Lütfen kart başlığını yazınız:");
+            _title = Console.ReadLine();
+            int todo;
+            int inProgress;
+            int done;
+
+            todo = _board.TODO.FindIndex(x => x.Title.ToLower() == _title.ToLower());
+            inProgress = _board.IN_PROGRESS.FindIndex(x => x.Title.ToLower() == _title.ToLower());
+            done = _board.DONE.FindIndex(x => x.Title.ToLower() == _title.ToLower());
+
+            //kart varsa çekip ekranda göster
+            if (todo >= 0)
+            {
+                _line = "TODO";
+                index = todo;
+            }
+            else if (inProgress >= 0)
+            {
+                _line = "IN_PROGRESS";
+                index = inProgress;
+            }
+            else if (done >= 0)
+            {
+                _line = "DONE";
+                index = done;
+            }
+            else
+            {
+                Console.WriteLine("Aradığınız krtiterlere uygun kart board'da bulunamadı. Lütfen bir seçim yapınız.");
+                Console.WriteLine("* İşlemi sonlandırmak için : (1)");
+                Console.WriteLine("* Yeniden denemek için : (2)");
+                int _check = int.Parse(Console.ReadLine());
+
+                if (_check == 1)
+                {
+                    Page();
+                }
+                else if (_check == 2)
+                {
+                    moveCard();
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz işlem seçtiniz. İşlem sonlandırılıyor.");
+                    Page();
+                }
+            }
+
+            if(_line is not null)
+            {
+                _card = _board.GetProperty(_line).Find(x => x.Title.ToLower() == _title.ToLower());
+                Console.WriteLine("Bulunan Kart Bilgileri:");
+                Console.WriteLine("**************************************");
+                Console.WriteLine("Başlık      : {0}", _card.Title);
+                Console.WriteLine("İçerik      : {0}", _card.Content);
+                Console.WriteLine("Atanan Kişi : {0}", _members.all.Find(x => x.Id == _card.Id).FullName);
+                Console.WriteLine("Büyüklük    : {0}", ((Size)_card.Size).ToString());
+                Console.WriteLine("Line        : {0}", _line);
+
+                Console.WriteLine("Lütfen taşımak istediğiniz Line'ı seçiniz:");
+                Console.WriteLine("(1) TODO");
+                Console.WriteLine("(2) IN PROGRESS");
+                Console.WriteLine("(3) DONE");
+
+                int _check1 = int.Parse(Console.ReadLine());
+
+                if( _check1 == 1)
+                {
+                    _board.GetProperty(_line).RemoveAt(index);
+                    _board.TODO.Add(_card);
+                }
+                else if (_check1 == 2)
+                {
+                    _board.GetProperty(_line).RemoveAt(index);
+                    _board.IN_PROGRESS.Add(_card);
+                }
+                else if (_check1 == 3)
+                {
+                    _board.GetProperty(_line).RemoveAt(index);
+                    _board.DONE.Add(_card);
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz işlem seçtiniz. İşlem sonlandırılıyor.");
+                    Page();
+                }
+                getBoard();
+            }
+
+
+        }
+
 
 
     }
